b687f1dddeff78649b7e62aff63fab286413015b
Saqib Shakil <saqib.shakil@apparel21.com>
2017-09-25 15:26:34 +0500

    Pesistant state

--- resources/js/configureStore.js
+++ resources/js/configureStore.js
@@ -1,11 +1,22 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import cards from './reducers/cards'
-
+import throttle from 'lodash/throttle'
 export default function configureStore(preloadedState) {
-
-  return createStore(
+  const session = sessionStorage.getItem('sessionState')
+  const state = session && JSON.parse(session)
+  let store = createStore(
     cards,
-    preloadedState,
+    state || preloadedState,
     window['__REDUX_DEVTOOLS_EXTENSION__']()
   )
+
+  store.subscribe(
+    throttle(
+      p => {
+        sessionStorage.setItem('sessionState', JSON.stringify(store.getState())
+          ,
+          1000)
+      })
+  )
+  return store
 }
